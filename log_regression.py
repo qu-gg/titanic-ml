25ff6df536424ff6e04dfa797c2a1d64ec1b9e5d
Ryan Missel <rxm7244@rit.edu>
2019-05-18 03:57:56 -0400

    Full vectorization - accuracy on test around 76.5%

--- log_regression.py
+++ log_regression.py
@@ -18,7 +18,7 @@ w = np.ones([NUM_FEATURES, 1])
 b = np.ones([1])
 
 # Learning rate
-lr = .002
+lr = .0005
 
 
 def sigmoid(x):
@@ -31,14 +31,6 @@ def loss_funct(y, y_hat):
     return -(np.multiply(y, np.log(y_hat)) + np.multiply((1.0 - y), np.log(1.0 - y_hat)))
 
 
-def backprop(x, y, y_hat):
-    # calculated derivative for regression, applied to the weights/bias
-    dldz = y_hat - y
-    dw = np.multiply(x, dldz)
-    db = dldz
-    return dw, db
-
-
 def test(w, b):
     # tests the accuracy of the network as a ratio of correct guesses over total
     Z = np.dot(w.T, dataset.T) + b
@@ -49,80 +41,56 @@ def test(w, b):
     print("Accuracy: {}".format(cor / len(dataset)))
 
 
-def vectorized_cost(A, w, b):
-    start = time.time()
-
+def vectorized_cost(w, b):
     m = len(dataset) - 1
+
+    # Making preds over the whole dataset
+    Z = np.dot(w.T, dataset.T) + b
+    A = sigmoid(Z)
 
     J = - (np.multiply(labels, np.log(A)) + np.multiply(1.0 - labels, np.log(1.0 - A)))
     J = J.sum()
 
     dZ = np.subtract(A.T, labels)
     dW = np.multiply(dataset, dZ).sum(axis=0)
+    dW = np.reshape(dW, [1, 4])
     dB = dZ.sum()
-    end = time.time()
 
-    print("Vectorized: ", J.sum() / m, dW / m, dB / m, "Time: ", end - start)
-    return J.sum() / m, dW / m, dB / m
-
-
-def cost_funct(w, b):
-    # cost function of the model, taking the average of all gradients and losses
-    m = len(dataset) - 1
-    dw = db = J = 0
-
-    # vectorized
-    Z = np.dot(w.T, dataset.T) + b
-    A = sigmoid(Z)
-    vectorized_cost(A, w, b)
-
-    # explicit for
-    for i in range(m):
-        x = np.reshape(dataset[i], [NUM_FEATURES, 1])
-        y = labels[i]
-
-        y_hat = sigmoid(np.matmul(w.T, x) + b)
-        J += loss_funct(y, y_hat)
-
-        tup = backprop(x, y, y_hat)
-        dw += tup[0]
-        db += tup[1]
-
-    cost = J / m
-    dw = dw / m
-    db = db / m
-    return cost, dw, db
+    return J / m, dW / m, dB / m
 
 
 def test_pred(w, b):
     # tests on the test set and builds the relevant csv submission file
     test = pd.read_csv("data/test_set.csv").to_numpy()
     test_ids = pd.read_csv("data/test_ids.csv").to_numpy()
+    test_ids = np.append([892], test_ids)
 
-    col = {"PassengerId": list(), "Survived": list()}
-
-    # Attempt at vectorized version
+    # Calculating test results
     Z = np.dot(w.T, test.T) + b
     A = sigmoid(Z)
+    A = np.rint(A.T)
+    A = A.astype(np.int)
+    A = np.reshape(A, [418])
 
-    col["PassengerId"] = test_ids
-    col["Survived"] = A.T
-    col["Survived"] = col["Survived"].round()
-
+    # Outputting to CSV
     name = input("Enter submission file name: ")
-    df = pd.DataFrame(col)
-    df.to_csv("submissions/{}.csv".format(name), index=False)
 
+    col = {"PassengerId": test_ids.tolist(), "Survived": A.tolist()}
+    df = pd.DataFrame.from_dict(col)
+    df.to_csv("submissions/{}.csv".format(name), index=False)
 
 
 # Training loop
 test(w, b)
 
-for _ in range(1):
+for epoch in range(10):
+    print("Epoch: ", epoch)
+
     # Shuffling the dataset in unison
     indices = np.arange(dataset.shape[0] - 1)
     np.random.shuffle(indices)
 
+    # Vectorized implementation
     dataset = dataset[indices]
     labels = labels[indices]
 
@@ -134,15 +102,17 @@ for _ in range(1):
         loss = loss_funct(y, y_hat)
 
         # Compute gradients, update weights
-        cost, dw, db = cost_funct(w, b)
+        cost, dw, db = vectorized_cost(w, b)
 
-        w = w - np.multiply(lr, dw)
-        b = b - np.multiply(lr, db)
+        w = w - np.multiply(lr, dw.T)
+        b = b - np.multiply(lr, db.T)
 
         if i % 800 == 0:
             print("Loss at step {}: {}".format(i, loss))
             print("Cost: {}".format(cost))
-        break
+
+
+print("Weights: ", w.T, "Bias: ", b)
 
 test(w, b)
 test_pred(w, b)
